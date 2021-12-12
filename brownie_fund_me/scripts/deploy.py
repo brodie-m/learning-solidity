c6e32fa6169cbad2367cc81e26ed7725e8e794bf
brodie-m <brodiedmcguire@gmail.com>
2021-12-12 14:17:28 +0000

    ganache setup & refactoring

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,10 +1,18 @@
-from brownie import FundMe
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+
+    # if we are on persistent network, use the address
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active(
+        )]["eth_usd_price_feed"]
+    else:
+        deploy_mocks()
+    fund_me = FundMe.deploy(MockV3Aggregator[-1].address, {
+                            "from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
 
 
