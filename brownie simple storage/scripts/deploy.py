7c04f2267f14507668a8b2f0ad887eba89931486
brodie-m <brodiedmcguire@gmail.com>
2021-12-12 13:03:34 +0000

    brownie

---
+++ brownie simple storage/scripts/deploy.py
@@ -0,0 +1,17 @@
+from brownie import accounts, config, SimpleStorage
+
+
+def deploy_simple_storage():
+    account = accounts[0]
+    # account = accounts.add(config["wallets"]["from_key"])
+    print(account)
+    simple_storage = SimpleStorage.deploy({"from":account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15,{"from":account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
+
+def main():
+    deploy_simple_storage()
