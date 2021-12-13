252d3890f752e9a84915e19d53acb8bc060dd29f
brodie-m <brodiedmcguire@gmail.com>
2021-12-13 13:52:47 +0000

    unit tesT

--- nft/scripts/deploy_and_create.py
+++ nft/scripts/deploy_and_create.py
@@ -5,10 +5,15 @@ token_uri = "ipfs://QmXbVFrGMuA2x6qjvpaPuBmLSugT1BSUdAZCsdwphNYMtm"
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
-def main():
+def deploy_and_create():
     account = get_account()
     simple_collectible = SimpleCollectible.deploy({"from": account})
     tx = simple_collectible.createCollectible(token_uri, {"from": account})
     tx.wait(1)
     print(
         f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
+    return simple_collectible
+
+
+def main():
+    deploy_and_create()
