d14b940631411159128c0242c165f2177166c1de
brodie-m <brodiedmcguire@gmail.com>
2021-12-14 15:49:10 +0000

    all done before testing

--- upgrades/scripts/deploy_and_upgrade.py
+++ upgrades/scripts/deploy_and_upgrade.py
@@ -1,5 +1,5 @@
-from scripts.helpful_scripts import get_account, encode_function_data
-from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy, Contract
+from scripts.helpful_scripts import get_account, encode_function_data, upgrade
+from brownie import network, Box, BoxV2, ProxyAdmin, TransparentUpgradeableProxy, Contract
 
 
 def main():
@@ -18,4 +18,12 @@ def main():
     print(f'Proxy deployed to {proxy}, you can now upgrade to v2!')
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
     proxy_box.store(1, {"from": account})
+    # upgrade
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade_tx = upgrade(account, proxy, box_v2.address,
+                         proxy_admin_contract=proxy_admin)
+    upgrade_tx.wait(1)
+    print('proxy has been upgraded!')
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    proxy_box.increment({"from": account})
     print(proxy_box.retrieve())
