edc826c273c3f79f6ad4e4804c392232457b846e
brodie-m <brodiedmcguire@gmail.com>
2021-12-15 11:34:55 +0000

    cleaning up cra

--- stake_yield_brownie/dbrod_token_farm/src/index.tsx
+++ stake_yield_brownie/dbrod_token_farm/src/index.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { DAppProvider, ChainId } from '@usedapp/core';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DAppProvider config ={{
+    supportedChains:[ChainId.Kovan,ChainId.Rinkeby, 1337]
+  }}>
+
     <App />
-  </React.StrictMode>,
+  </DAppProvider>,
+  
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
