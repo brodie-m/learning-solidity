aa9ab6c5705b6e8cc21ce87078cb117687375d9e
brodie-m <brodiedmcguire@gmail.com>
2021-12-15 13:36:00 +0000

    adding hooks

--- stake_yield_brownie/dbrod_token_farm/src/Components/YourWallet/StakeForm.tsx
+++ stake_yield_brownie/dbrod_token_farm/src/Components/YourWallet/StakeForm.tsx
@@ -1,7 +1,7 @@
 import { formatUnits } from '@ethersproject/units'
 import { Button, Input } from '@material-ui/core'
 import { useEthers, useTokenBalance } from '@usedapp/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { Token } from '../Main'
 
 export interface StakeFormProps {
@@ -9,13 +9,25 @@ export interface StakeFormProps {
 }
 
 const StakeForm = ({ token }: StakeFormProps) => {
+    const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
     const { address: tokenAddress, name } = token
     const { account } = useEthers()
     const tokenBalance = useTokenBalance(tokenAddress, account)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
+    
+    
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const newAmount = e.target.value === ""?"" : Number(e.target.value)
+        setAmount(newAmount)
+    }
+
+    function handleClick(e: React.ChangeEvent) {
+
+    }
+    
     return (
         <>
-            <Input />
+            <Input onChange={handleChange}/>
             <Button color='secondary' variant='contained'>
                 stake!
             </Button>
