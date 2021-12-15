d597b2d4161130ee3c01eabeae7b7f940b9252f3
brodie-m <brodiedmcguire@gmail.com>
2021-12-15 12:50:43 +0000

    displaying cool stuff for each token

--- stake_yield_brownie/dbrod_token_farm/src/Components/YourWallet/WalletBalance.tsx
+++ stake_yield_brownie/dbrod_token_farm/src/Components/YourWallet/WalletBalance.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Token } from '../Main'
 import { useEthers, useTokenBalance } from '@usedapp/core'
 import { formatUnits } from 'ethers/lib/utils'
+import BalanceMsg from '../BalanceMsg'
 export interface WalletBalanceProps {
     token: Token
 }
@@ -12,9 +13,11 @@ const WalletBalance = ({ token }: WalletBalanceProps) => {
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
 
     return (
-        <div>
-            {formattedTokenBalance}
-        </div>
+        <BalanceMsg
+            label={`your un-staked ${name} balance`}
+            tokenImgSrc={image}
+            amount={formattedTokenBalance}
+        />
     )
 }
 
